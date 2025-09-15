78d7004ffff87a0742d60ffb875ca1a8b86ed1fc
dwiluthfianto <dwiluthfianto72@gmail.com>
2025-09-15 15:49:26 +0700

    config: change config to register unsplash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
